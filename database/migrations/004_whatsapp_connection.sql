2debb804af5f1a682eb8e698c16e58d33aa0053f
Ismaias Mathyas <mathyas2026floragold@gmail.com>
2026-07-21 22:33:38 -0300

    Delete database/migrations/004_whatsapp_connection.sql

--- database/migrations/004_whatsapp_connection.sql
+++
@@ -1,6 +0,0 @@
--- Upgrade para instalações que já aplicaram 001_schema.sql.
-alter table whatsapp_connections add column if not exists phone_number text;
-alter table whatsapp_connections add column if not exists created_at timestamptz not null default now();
-alter table whatsapp_connections drop constraint if exists whatsapp_connections_status_check;
-alter table whatsapp_connections add constraint whatsapp_connections_status_check
-  check(status in('offline','connecting','connected'));
